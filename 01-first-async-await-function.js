f9eb0fa200bf90de0622c4014c427fbac4260bfc
wangshijun2010 <wangshijun2010@gmail.com>
2017-09-08 07:03:57 +0800

    lesson 01: first async/await function

--- 01-first-async-await-function.js
+++ 01-first-async-await-function.js
@@ -1,13 +1,11 @@
 const fetch = require('node-fetch');
 
-function getZhihuColumn(id) {
+async function getZhihuColumn(id) {
   const url = `https://zhuanlan.zhihu.com/api/columns/${id}`;
-  fetch(url)
-    .then(response => response.json())
-    .then(column => {
-      console.log(`NAME: ${column.name}`);
-      console.log(`INTRO: ${column.intro}`);
-    });
+  const response = await fetch(url);
+  const column = await response.json();
+  console.log(`NAME: ${column.name}`);
+  console.log(`INTRO: ${column.intro}`);
 }
 
 getZhihuColumn('feweekly');
